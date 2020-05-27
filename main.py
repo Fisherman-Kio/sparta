41b1e2d676d88919ad3f1c9d4bb09dff432387c4
Kio <un19451024@gmail.com>
2020-05-27 13:34:13 +0000

    test3

--- main.py
+++ main.py
@@ -5,8 +5,31 @@ client = MongoClient(url)
 db = client['heroku_33krt5nm']
 collection = db['test']
 
-collection.insert_one({'a':'b'})
+# collection.insert_one({'name': 'bobby', 'age': 21})
+# collection.insert_one({'name': 'kay', 'age': 27})
+# collection.insert_one({'name': 'john', 'age': 30})
+# collection.insert_one({'name': 'patrick', 'age': 72})
 
 rows = collection.find({})
+
+# collection.delete_many({'name':'bobby','age':21})
+
+result = []
+
 for row in rows:
-  print(row)
+  if 'age' in row: # 꼭 필요
+    if row['age'] > 21 :
+      result.append(row['age'])
+# 이렇게 써야 공부가 됨 파이썬식
+
+max_age = max(result)
+
+rows = collection.find()
+
+count_number = len(result)
+
+for row in rows:
+  if 'age' in row: 
+    if row['age'] == max_age :
+      print(count_number)
+
